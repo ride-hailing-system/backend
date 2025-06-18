7e9a9e2e01022130722fb8a6378726c45dd108c7
surafelhabte1@gmail.com <surafelhabte1@gmail.com>
2025-06-18 15:55:58 +0300

    Add authentication mutations and queries; implement password reset and change functionality

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,9 +1,11 @@
 import { GraphQLObjectType } from "graphql";
 import { userQueries } from "./queries/userQueries";
+import { authQueries } from "./queries/authQueries";
 
 export const Query = new GraphQLObjectType({
   name: "Query",
   fields: {
     ...userQueries,
+    ...authQueries
   },
 });
